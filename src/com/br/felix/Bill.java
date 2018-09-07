b7ca9f70ecbddb02cd179b815f5b2b3b5ba62ed7
elfefe <f.bou-reiff@orange.fr>
2018-09-07 22:44:30 +0200

    Ajout des sous classes Product: Fridge et Television

--- src/com/br/felix/Bill.java
+++ src/com/br/felix/Bill.java
@@ -1,4 +1,20 @@
 package com.br.felix;
 
+import java.util.Map;
+
 public class Bill {
+    private Customer customer;
+    private Map<Product, Integer> product;
+
+    public void addProduct(Product product, Integer quantity){
+        this.product.put(product,quantity);
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Map<Product, Integer> getProduct() {
+        return product;
+    }
 }
