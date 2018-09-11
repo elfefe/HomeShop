168ae3cd88bb5a220a0ae335b14abbe6b0029598
Félix <f.bou-reiff@orange.fr>
2018-09-11 15:33:36 +0200

    Ajout du package dao et de la librairie maven.bdd.

---
+++ src/main/java/com/br/felix/dao/ProductDAO.java
@@ -0,0 +1,11 @@
+package com.br.felix.dao;
+
+import com.br.felix.Product;
+
+import java.util.List;
+
+public class ProductDAO {
+    public List<Product> getAll() {
+        return null;
+    }
+}
